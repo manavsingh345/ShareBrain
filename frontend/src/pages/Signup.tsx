c94460cc92ba008776bd96f0914a9dba3094e996
Manav Singh <manavchaudhary060705@gmail.com>
2025-08-05 18:06:50 +0530

    Signup

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
