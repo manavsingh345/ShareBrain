46455345992f3c54b62dc70498bc5bae877bec09
Manav Singh <manavchaudhary060705@gmail.com>
2025-09-04 09:02:33 +0530

    ChatBot

--- frontend/src/pages/ChatBot.tsx
+++ frontend/src/pages/ChatBot.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./chat.css"
 export default function(){
+    const [message,setMessage]=useState("");
+    const [reply,setReply]=useState("");
+    const getReply=async ()=>{
+        if (!message) return;
+        const payload={message};
+        const options = {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(payload),
+        };
+        try{
+            const response=await fetch("http://localhost:3000/api/v1/chat",options);
+            const data=await response.json();
+            console.log(data);
+            setReply(data.reply);
+        }catch(e){
+            console.log(e);
+        }
+    }
     return(
         <div className="chat">
-            <div><p>hello</p></div>
-            <input type="text" placeholder="Enter your message" className="absolute bottom-0 w-full bg-black text-white p-2" />
-            <span className="absolute bottom-0 right-0 text-white pb-2 pr-2 cursor-pointer"><i className="fa-solid fa-paper-plane"></i></span>
+            <div><p>{reply}</p></div>
+            <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} onKeyDown={(e)=> e.key ==='Enter'? getReply():""} placeholder="Enter your message" className="absolute bottom-0 w-full bg-black text-white p-2" />
+            <span className="absolute bottom-0 right-0 text-white pb-2 pr-2 cursor-pointer" onClick={getReply}><i className="fa-solid fa-paper-plane"></i></span>
         </div>
     )
 }
+
+
