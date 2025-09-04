c5f1b5a49a1e052cf96cf58599351aea46c7903d
Manav Singh <manavchaudhary060705@gmail.com>
2025-09-04 15:13:46 +0530

    ChatBot

--- frontend/src/pages/ChatBot.tsx
+++ frontend/src/pages/ChatBot.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./chat.css"
+type ChatMessage = {
+    sender:"user" | "bot",
+    text:string
+};
+
 export default function(){
     const [message,setMessage]=useState("");
-    const [reply,setReply]=useState("");
+    const [chats,setChats]=useState<ChatMessage[]>([]);
+
     const getReply=async ()=>{
         if (!message) return;
+        setChats((prev) => [...prev, { sender: "user", text: message }]);
+
         const payload={message};
         const options = {
             method:"POST",
@@ -17,14 +25,22 @@ export default function(){
             const response=await fetch("http://localhost:3000/api/v1/chat",options);
             const data=await response.json();
             console.log(data);
-            setReply(data.reply);
+            setChats((prev)=>[...prev,{sender:"bot",text:data.reply}]);
         }catch(e){
             console.log(e);
         }
+        setMessage(""); 
     }
     return(
-        <div className="chat">
-            <div><p>{reply}</p></div>
+        <div className="chat flex flex-col">
+            <div className="flex-1 overflow-y-auto p-4 flex flex-col">
+                {
+                    chats.map((chat,idx)=>(
+                        <p  key={idx} className={`p-2 rounded-lg max-w-[80%] ${chat.sender === "user" ? "bg-blue-600 self-end" : "bg-gray-700 self-start" }`}>{chat.text}</p>
+                    ))
+                }
+            </div>
+
             <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} onKeyDown={(e)=> e.key ==='Enter'? getReply():""} placeholder="Enter your message" className="absolute bottom-0 w-full bg-black text-white p-2" />
             <span className="absolute bottom-0 right-0 text-white pb-2 pr-2 cursor-pointer" onClick={getReply}><i className="fa-solid fa-paper-plane"></i></span>
         </div>
