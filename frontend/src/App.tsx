5bf283c0613dc253df9e865252f72eb226e46817
Manav Singh <manavchaudhary060705@gmail.com>
2025-10-27 22:59:05 +0530

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Dashboard/>}></Route>
       <Route path='/signup' element={<Signup/>}></Route>
       <Route path='/signin' element={<Signin/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
     </Routes>
     </BrowserRouter>
   )
