1ed8e8053bf08f87908797a7ebe69ce4f8b2f20b
Manav Singh <manavchaudhary060705@gmail.com>
2025-11-08 10:14:56 +0530

    Dash

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { Signin } from './pages/Signin'
-import { Dashboard } from './pages/Dashboard'
+// import { Dashboard } from './pages/Dashboard'
 import { Signup } from './pages/Signup'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Dash from './dash/Dash'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Dashboard/>}></Route>
+      {/* <Route path='/' element={<Dashboard/>}></Route> */}
+      <Route path='/' element={<Dash/>}></Route>
       <Route path='/signup' element={<Signup/>}></Route>
       <Route path='/signin' element={<Signin/>}></Route>
     </Routes>
