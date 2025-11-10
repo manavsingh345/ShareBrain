908659a188e0f8a5598c69f4be8af8b0882be4a7
Manav Singh <manavchaudhary060705@gmail.com>
2025-11-10 15:54:23 +0530

    Card

--- frontend/src/dash/Card.tsx
+++ frontend/src/dash/Card.tsx
@@ -8,7 +8,7 @@ interface Info{
 
 export default function Card(props:Info){
     return(
-        <div className="m-1 h-58 w-94  bg-gradient-to-br from-violet-600 to-indigo-100  border-none rounded-xl">
+        <div className="m-1 h-58 w-94  bg-[#101428]  border-none rounded-xl">
             <div className="h-12 w-12 text-white ml-5 mt-5 mb-5 ">{props.icon}</div>
             <div className="ml-5 flex flex-col">
             <div className="text-xl font-bold text-white">{props.heading}</div>
