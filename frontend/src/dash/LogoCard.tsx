3c4d49ab137d2794a2611c53ad7a482affee9d00
Manav Singh <manavchaudhary060705@gmail.com>
2025-11-11 17:15:32 +0530

    Dashboard

--- frontend/src/dash/LogoCard.tsx
+++ frontend/src/dash/LogoCard.tsx
@@ -1,14 +1,14 @@
-// import type { ReactElement } from "react"
+import type { ReactElement } from "react"
 
 interface Property{
     text:string,
-    // icon:ReactElement,
+    icon?:ReactElement,
     width?:string,
     height?:string
 }
 export default function LogoCard(props:Property){
     return (
-        <div className="bg-[#101428]" style={{height:props.height || "12px",width:props.width || "30px"}}>
+        <div className="bg-[#101428] m-2 flex justify-center items-center border border-gray-800 rounded-xl" style={{height:props.height || "50px",width:props.width || "300px"}}>
             {props.text}
         </div>
     )
--- frontend/src/dash/SmallCard.tsx
+++ frontend/src/dash/SmallCard.tsx
@@ -1,19 +1,30 @@
 import LogoCard from "./LogoCard";
 
+export default function SmallCard() {
+  return (
+    <div className="bg-black min-h-screen">
+      <div className="flex justify-center items-center text-3xl font-bold text-white py-10 pt-20">
+        Works with All Your Content
+      </div>
 
-export default function SmallCard(){
-    return (
-        <div className="bg-black min-h-screen">
-         <div className="flex justify-center items-center text-3xl font-bold text-white  py-10 pt-20">
-            
-            Works with All Your Content
-            
-            <div className="flex justify-center items-start">
-                <LogoCard text={"jvbc scknskn dkvcnksn"}/>
-            </div>
-            
-        </div>
+      
+      <div className="flex flex-col items-center gap-2 text-white">
         
+      
+        <div className="flex justify-center flex-wrap gap-1">
+          <LogoCard text={"YouTube, TikTok, IG Reels"} />
+          <LogoCard text={"PDFs & Documents"} />
+          <LogoCard text={"Websites & Articles"} />
         </div>
-    )
+
+        
+        <div className="flex justify-center flex-wrap gap-1">
+          <LogoCard text={"Audio & Podcasts"} />
+          <LogoCard text={"Code & GitHub Repos"} />
+          <LogoCard text={"Google Docs/Sheets/Slides"} />
+          <LogoCard text={"LinkedIn Posts, Carousels, Videos"} />
+        </div>
+      </div>
+    </div>
+  );
 }
